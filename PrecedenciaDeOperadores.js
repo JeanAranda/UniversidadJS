81668424a218c56980ea33829925502a78c2c84d
JeanAranda <jarandar2014@gmail.com>
2024-05-12 17:39:33 -0500

    Se sube precedencia de operadores

---
+++ PrecedenciaDeOperadores.js
@@ -0,0 +1,17 @@
+//Precedencia de Operadores
+//1.Parentesis y corchetes
+//2.Operadores unarios, como -, ++, --, !
+//3.Aritmeticos *, / y %
+//4.Aritmeticos + y -
+//5.Relacionales <, <=, > y >=
+//6.Igualdad == y !=
+//7.Logicos && y || 
+//8.Asignacion =, =+, -=, *=, etc
+
+//Ejemplo una expresion se revisa de derecha a izquierda
+let a = 12/3+2*3-1
+//Paso 1 Division 12/3
+//Paso 2 Multiplicacion 2*3
+//Paso 3 Suma 6
+//Paso 4 Resta 10-1
+console.log(a)
