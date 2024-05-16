e46d24b0d2ad98f36c1b66e96cbf05e8fb07286d
JeanAranda <jarandar2014@gmail.com>
2024-05-15 22:42:18 -0500

    Se avanza hasta else if

---
+++ SentenciaIf.js
@@ -0,0 +1,7 @@
+//Sentencia if
+let miNumero = 10;
+//Revisar si el numero es positivo
+if (miNumero > 0) {
+  console.log(`Valor positivo ${miNumero}`);
+}
+//esto es solo para entender la sentencia if que termina en la linea 5
---
+++ SentenciaIfElse.js
@@ -0,0 +1,10 @@
+//Sentencia if else
+let miNumero = -10;
+//Revisar si el numero es positivo
+if (miNumero > 0) {
+  console.log(`Valor positivo ${miNumero}`);
+} else if (miNumero < 0) {
+  console.log(`Valor negativo ${miNumero}`);
+} else {
+  console.log(`Valor cero ${miNumero}`);
+}
