bcb53294a08cdcaa9b5733b58b1fbd1cbf0b204d
JeanAranda <jarandar2014@gmail.com>
2024-10-06 18:23:27 -0500

    ordenamieno¿to  de archivos y carpetas

--- CicloWhile.js
+++
@@ -1,8 +0,0 @@
-//Ciclo while
-//Imprimir los valores del 0 al 5
-//Contador con ciclo while
-let contador = 0, repeticiones = 5;
-while (contador <= repeticiones) {
-    console.log(contador++);
-    //contador++
-}
