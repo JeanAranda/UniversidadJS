87c34f5d00c01ce849a49b7125f4172742f123ef
JeanAranda <jarandar2014@gmail.com>
2024-10-08 21:22:17 -0500

    Se sube matrices

---
+++ MATRICES/EjempleDeMatrices.js
@@ -0,0 +1,16 @@
+//Matrices en JS
+//Declaracion de una matriz
+let matriz = [[],[]]
+//Modificar los valorews
+//Renglon 0
+matriz [0][0] = 100
+matriz [0][1] = 200
+matriz [0][2] = 300
+//Renglon 1
+matriz [1][0] = 400
+matriz [1][1] = 500
+matriz [1][2] = 600
+//Leer los valores
+console.log(`Elemento [0][1] = ${matriz[0][1]}`)
+console.log(`Elemento [0][1] = ${matriz[1][2]}`)
+
